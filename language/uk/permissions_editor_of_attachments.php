5ed933e13d075b736f5f9a1d82b33e60d85b0c56
fryschyn <fryschyn@gmail.com>
2014-07-23 17:40:54 +0300

    Ukrainian language

---
+++ language/uk/permissions_editor_of_attachments.php
@@ -0,0 +1,27 @@
+<?php
+/**
+*
+* quickreply [Ukrainian]
+*
+* @package language quickreply
+* @copyright (c) 2013 Oleksii Frychyn (Sherlock)
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'ACL_A_EDITOR_ATTACH'			=> 'Може змінювати розширені налаштування вкладень',
+));
